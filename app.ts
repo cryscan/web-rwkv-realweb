0024882ff071aa8b4f6bbf8f5387659a4e3ba7a3
cryscan <cryscan@umich.edu>
2024-02-18 23:06:46 +0800

    Reset default link.

--- app.ts
+++ app.ts
@@ -1,42 +1,11 @@
-function download(url: string, progress: ((this: XMLHttpRequest, ev: ProgressEvent<EventTarget>) => any) | null): Promise<Response> {
-    return new Promise((res, rej) => {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'blob';
-        xhr.onprogress = progress;
-        xhr.onload = function (e) {
-            if ((xhr.status >= 200 && xhr.status < 300) || (xhr.status === 0 /* Loaded from local file */)) {
-                res(this.response);
-            } else if (xhr.status === 404 && url.startsWith('http://localhost')) {
-                rej(`Model not found locally.  Please download and place in the assets/models/ folder.  Make sure you are running on port 8080`);
-            } else {
-                console.log('onload: Loading model error', xhr, e);
-                rej(`Loading model error: ${xhr.status}: ${xhr.statusText}`);
-            }
-        };
-        xhr.onerror = function (e) {
-            console.log('onerror: Loading model error', xhr, e);
-            if (xhr.status === 0 && !xhr.statusText) {
-                // There's no information to show
-                // https://bugs.chromium.org/p/chromium/issues/detail?id=118096
-                rej('Failed to load the model from Hugging Face. Might be related to a problem on their end.  Please try waiting a bit and then trying again, or use local copy (see README).')
-            } else {
-                rej(`Loading model error: ${xhr.status}: ${xhr.statusText}`);
-            }
-        }
-        xhr.send();
-    });
-}
-
 async function load() {
-    var url = (document.getElementById("url") as HTMLInputElement).value;
-
     const modelElem = document.getElementById("model")!;
     const downloadElem = document.getElementById("download")!;
     const progressElem = document.getElementById("progress") as HTMLProgressElement;
     const statusElem = document.getElementById("status")!;
     const chatElem = document.getElementById("chat")!;
     const replyElem = document.getElementById("reply")!;
+    var url = (document.getElementById("url") as HTMLInputElement).value;
 
     modelElem.style.display = "none";
     downloadElem.style.display = "";
