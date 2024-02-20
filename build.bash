e72541f19e89357586e933bc27b2b4d873ef2cdd
研究社交 <cryscan@umich.edu>
2024-02-20 11:40:13 +0800

    Fix build.bash.

--- build.bash
+++ build.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build wasm
 echo "== building wasm =="
-RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --release --target no-modules
+RUSTFLAGS=--cfg=web_sys_unstable_apis wasm-pack build --release --target no-modules
 # Build typescript
 echo "== building typescript =="
 npx tsc
