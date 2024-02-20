d4a51e3e81f883d393dcd022e75f5a291f16962c
研究社交 <cryscan@umich.edu>
2024-02-20 11:31:48 +0800

    Update build.bash

--- build.bash
+++ build.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Set RUSTFLAGS environment variable
-RUSTFLAGS="--cfg=web_sys_unstable_apis"
 # Build wasm
 echo "== building wasm =="
-cargo build --release --target no-modules
+RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --release --target no-modules
 # Build typescript
 echo "== building typescript =="
 npx tsc
